a0f3ec2a0ffd78ee7b298fe9859f32fa0c51556e
João Victor <jvsouzax@gmail.com>
2020-04-01 18:15:18 -0300

    add: navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React from 'react'
 
-function App() {
+import Topbar from './components/Topbar'
+
+export default function App() {
   return (
-    <>
-    </>
-  );
+    <Topbar />
+  )
 }
-
-export default App;
---
+++ src/components/Topbar/index.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { AiFillInfoCircle } from 'react-icons/ai'
+import styled from 'styled-components'
+
+import brandImg from '../../assets/obc-icon.png'
+
+export default function Topbar() {
+  function AboutModal() {
+    alert('click')
+  }
+
+  return (
+    <Nav>
+      <Brand>
+        <BrandImg src={brandImg} alt="Logo da One Bit Code" />
+        <BrandText>One Bit Code</BrandText>
+      </Brand>
+
+      <AboutBtn onClick={AboutModal} >
+        <AiFillInfoCircle /> Sobre nós
+      </AboutBtn>
+    </Nav>
+  )
+}
+
+const Nav = styled.nav`
+  background-color: #2B2B2B;
+  width: 100%;
+  height: 56px;
+  justify-content: space-around;
+  align-items: center;
+  display: flex;
+`
+
+const Brand = styled.div`
+  display: flex;
+`
+
+const BrandText = styled.div`
+  margin: auto;
+  color: #ffffff;
+  font-size: 16px;
+  padding-left: 8px;
+`
+
+const BrandImg = styled.img`
+  width: 46px;
+`
+
+const AboutBtn = styled.button`
+  color: #ffffff;
+  background: none;
+  border: none;
+  font-size: 16px;
+`

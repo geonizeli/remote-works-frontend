5b6129ea75ac805d54bff5d50b1f7bce705307fe
João Victor <jvsouzax@gmail.com>
2020-04-01 18:46:10 -0300

    add: page jobs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React from 'react'
 
-import Topbar from './components/Topbar'
-import Footer from './components/Footer'
-import MessengerShortcut from './components/MessengerShortcut'
+import Jobs from './pages/Jobs'
 
 export default function App() {
   return (
-    <>
-      <Topbar />
-      <Footer />
-      <MessengerShortcut />
-    </>
+    <Jobs />
   )
 }

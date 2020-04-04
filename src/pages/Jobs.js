58cebfb953e6694a20438bea23211142b8616425
João Victor <jvsouzax@gmail.com>
2020-04-03 22:54:36 -0300

    add: autocomplete component

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -4,13 +4,35 @@ import styled from 'styled-components'
 import Topbar from '../components/Topbar'
 import Footer from '../components/Footer'
 import MessengerShortcut from '../components/MessengerShortcut'
+import Autocomplete from '../components/Autocomplete'
 
 import Card from '../components/Card'
 
 export default function Jobs() {
+  const suggestions = [
+    "Alligator",
+    "Bask",
+    "Crocodilian",
+    "Death Roll",
+    "Eggs",
+    "Jaws",
+    "Reptile",
+    "Solitary",
+    "Tail",
+    "Wetlands"
+  ]
+
   return (
     <>
       <Topbar />
+
+      <Filter>
+        <form>
+          <Autocomplete
+            suggestions={suggestions}
+          />
+        </form>
+      </Filter>
 
       <JobsContainer>
         {[0, 1, 2 , 3, 4].map(job => (
@@ -34,4 +56,11 @@ const JobsContainer = styled.div`
   margin: auto;
   padding-top: 24px;
   min-height: calc(100vh - 80px - 56px);
+`
+
+const Filter = styled.div`
+  text-align: center;
+  min-height: 150px;
+  margin-top: 24px;
+
 `
